266c8311d518ee3c1da7a43fde9cda1f594d9abc
ivaylo-milanov <imilanov24@gmail.com>
2024-02-25 15:59:34 +0200

    Added header and pop-up components

--- 02. Components/todo-app/src/app/app.component.ts
+++ 02. Components/todo-app/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoListComponent } from "./todo-list/todo-list.component";
+import { HeaderComponent } from "./header/header.component";
+import { PopUpComponent } from './pop-up/pop-up.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, TodoListComponent, HeaderComponent, PopUpComponent]
 })
 export class AppComponent {
   title = 'todo-app';
--- 02. Components/todo-app/src/app/todo-list/todo-list.component.ts
+++ 02. Components/todo-app/src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-todo-list',
+  selector: 'todo-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
-
+  users: string[] = ['Shopping', 'Skiing'];
 }
